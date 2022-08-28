33179b4505efcd8772e5841fa2fe0109ae275210
Erik Ciau <erikciau777@icloud.com>
2022-08-28 18:27:11 -0500

    herro component and navbar added

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
     <Title>Agencia Kamisato - Fan Page</Title>
   </Head>
 
-  <Body class="bg-violet-200 max-w-7xl mx-auto">
+  <Body class="bg-violet-200 mx-auto">
     <div>
       <NuxtPage />
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,15 @@
+<script setup lang="ts">
+const isNavbarActive = ref(false)
+</script>
+
 <template>
-  <div>
-    <nav class="min-h-[8vh] flex justify-between items-center text-indigo-700 font-bold text-base uppercase">
-      <h2 class="font-['Concert_One'] text-3xl font-extrabold">
+  <div class="px-2 md:px-0">
+    <nav
+      class="sticky top-0 min-h-[8vh] flex justify-between items-center text-indigo-700 font-bold text-base uppercase select-none px-4 bg-violet-200 z-20">
+      <h2 class="font-['Concert_One'] text-xl md:text-3xl font-extrabold">
         <span>Agencia Kamisato</span>
       </h2>
-      <ul class="inline-flex gap-4">
+      <ul class="md:inline-flex gap-4 hidden">
         <li class="nav-button animation-text hover:text-white">
           <a href="#">Incio</a>
         </li>
@@ -19,16 +24,32 @@
         </li>
       </ul>
       <a href="#"
-        class="animation-text inline-flex justify-center items-center h-10 px-4 bg-violet-800 text-white hover:text-violet-800 hover:bg-white rounded-full hover:shadow-2xl hover:shadow-purple-400 gap-1">
+        class="hidden animation-text md:inline-flex justify-center items-center h-10 px-4 bg-indigo-700 text-white hover:text-indigo-700 hover:bg-white rounded-full hover:shadow-2xl hover:shadow-purple-400 gap-1">
         <span>Ingresa</span>
         <svg-door class="inline-block w-4 h-4" />
       </a>
+
+      <button @click="isNavbarActive = !isNavbarActive"
+        class="animation-text bg-trasparent md:hidden active:scale-110 active:bg-white inline-flex justify-center items-center p-2 rounded-full">
+        <svg-menu class="block w-7 h-7" />
+      </button>
+
+      <div
+        class="animation-text block md:hidden min-h-screen fixed top-0 right-0 bg-indigo-700 translate-x-0 w-10/12 rounded-l-lg p-4 text-violet-200"
+        :class="[isNavbarActive ? 'translate-x-0' : 'translate-x-full']">
+        
+      </div>
+
     </nav>
     <slot></slot>
   </div>
 </template>
 
 <style>
+* {
+  -webkit-tap-highlight-color: transparent;
+}
+
 .animation-text {
   @apply transition-all ease-linear duration-150;
 }
