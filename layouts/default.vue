c23259783a635717fcea4fbcd8bd465a4ec82d7d
Erik Ciau <erikciau777@icloud.com>
2022-08-30 11:41:45 -0500

    navbar in hero bottom

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,43 +4,46 @@ const isNavbarActive = ref(false)
 
 <template>
   <div class="px-2 md:px-0">
-    <nav
-      class="sticky top-0 min-h-[8vh] flex justify-between items-center text-indigo-700 font-bold text-base uppercase select-none px-4 bg-violet-200 z-20">
-      <h2 class="font-['Concert_One'] text-xl md:text-3xl font-extrabold">
-        <span>Agencia Kamisato</span>
-      </h2>
-      <ul class="md:inline-flex gap-4 hidden">
-        <li class="nav-button animation-text hover:text-white">
-          <a href="#">Incio</a>
-        </li>
-        <li class="nav-button animation-text hover:text-white">
-          <a href="#">Personajes</a>
-        </li>
-        <li class="nav-button animation-text hover:text-white">
-          <a href="#">Noticias</a>
-        </li>
-        <li class="nav-button animation-text hover:text-white">
-          <a href="#">Comunidad</a>
-        </li>
-      </ul>
-      <a href="#"
-        class="hidden animation-text md:inline-flex justify-center items-center h-10 px-4 bg-indigo-700 text-white hover:text-indigo-700 hover:bg-white rounded-full hover:shadow-2xl hover:shadow-purple-400 gap-1">
-        <span>Ingresa</span>
-        <svg-door class="inline-block w-4 h-4" />
-      </a>
+    <slot name="hero"></slot>
+    <div class="sticky top-0 px-4 py-1 mx-auto">
+      <nav
+        class="min-h-[8vh] flex justify-between items-center text-violet-200 font-bold text-base uppercase select-none px-4 bg-[#171a4a] z-20 rounded-xl shadow-xl">
+        <h2 class="font-['Concert_One'] text-xl md:text-3xl font-extrabold">
+          <span>Agencia Kamisato</span>
+        </h2>
+        <ul class="md:inline-flex gap-4 hidden">
+          <li class="nav-button animation-text hover:text-white">
+            <a href="#">Incio</a>
+          </li>
+          <li class="nav-button animation-text hover:text-white">
+            <a href="#">Personajes</a>
+          </li>
+          <li class="nav-button animation-text hover:text-white">
+            <a href="#">Noticias</a>
+          </li>
+          <li class="nav-button animation-text hover:text-white">
+            <a href="#">Comunidad</a>
+          </li>
+        </ul>
+        <a href="#"
+          class="hidden animation-text md:inline-flex justify-center items-center h-10 px-4 bg-indigo-700 text-white hover:text-indigo-700 hover:bg-white rounded-full hover:shadow-2xl hover:shadow-purple-400 gap-1">
+          <span>Ingresa</span>
+          <svg-door class="inline-block w-4 h-4" />
+        </a>
 
-      <button @click="isNavbarActive = !isNavbarActive"
-        class="animation-text bg-trasparent md:hidden active:scale-110 active:bg-white inline-flex justify-center items-center p-2 rounded-full">
-        <svg-menu class="block w-7 h-7" />
-      </button>
+        <button @click="isNavbarActive = !isNavbarActive"
+          class="animation-text bg-trasparent md:hidden active:scale-110 active:bg-white inline-flex justify-center items-center p-2 rounded-full">
+          <svg-menu class="block w-7 h-7" />
+        </button>
 
-      <div
-        class="animation-text block md:hidden min-h-screen fixed top-0 right-0 bg-indigo-700 translate-x-0 w-10/12 rounded-l-lg p-4 text-violet-200"
-        :class="[isNavbarActive ? 'translate-x-0' : 'translate-x-full']">
-        
-      </div>
+        <div
+          class="animation-text block md:hidden min-h-screen fixed top-0 right-0 bg-indigo-700 translate-x-0 w-10/12 rounded-l-lg p-4 text-violet-200"
+          :class="[isNavbarActive ? 'translate-x-0' : 'translate-x-full']">
 
-    </nav>
+        </div>
+
+      </nav>
+    </div>
     <slot></slot>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,24 @@
 <template>
   <NuxtLayout>
-    <header class="min-h-[92vh] flex justify-start items-center relative overflow-hidden">
-      <div class="flex-1 flex flex-col items-center justify-center text-center max-w-3xl mx-auto lg:mx-0 space-y-10">
-        <span class="uppercase text-5xl md:text-8xl font-black text-indigo-700">
-          <h2>Join the community</h2>
-        </span>
-        <ul class="flex justify-center items-center w-full gap-5">
-          <li class="animation-text p-2 hover:shadow-lg hover:bg-violet-200 rounded-xl text-indigo-700">
-            <svg-discord class="block w-14 h-14" />
-          </li>
-          <li class="animation-text p-2 hover:shadow-lg hover:bg-violet-200 rounded-xl text-indigo-700">
-            <svg-facebook class="block w-14 h-14" />
-          </li>
-        </ul>
-      </div>
-      <img src="/images/hero-2.webp" class="absolute blur-sm md:blur-none -z-10 bottom-0 right-0 h-full object-cover"
-        width="600" alt="">
-    </header>
+    <template #hero>
+      <header class="min-h-screen flex justify-start items-center relative overflow-hidden">
+        <div class="flex-1 flex flex-col items-center justify-center text-center max-w-3xl mx-auto lg:mx-0 space-y-10">
+          <span class="uppercase text-5xl md:text-8xl font-black text-indigo-700">
+            <h2>Join the Kamisato community</h2>
+          </span>
+          <ul class="flex justify-center items-center w-full gap-5">
+            <li class="animation-text p-2 hover:shadow-lg hover:bg-violet-200 rounded-xl text-indigo-700">
+              <svg-discord class="block w-14 h-14" />
+            </li>
+            <li class="animation-text p-2 hover:shadow-lg hover:bg-violet-200 rounded-xl text-indigo-700">
+              <svg-facebook class="block w-14 h-14" />
+            </li>
+          </ul>
+        </div>
+        <img src="/images/hero-2.webp" class="absolute blur-sm md:blur-none -z-10 bottom-0 right-0 h-full object-cover"
+          width="600" alt="">
+      </header>
+    </template>
     <section id="main" class="my-5">
       <header class="text-center space-y-4 text-indigo-800">
         <h2 class="text-4xl font-bold">Noticias</h2>
@@ -24,8 +26,7 @@
       </header>
       <div
         class="mt-10 mx-auto max-w-7xl px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-content-center place-items-center">
-        <a href="#"
-          class="block animation-text max-w-sm p-6 bg-indigo-500 rounded-xl shadow-2xl hover:bg-indigo-600"
+        <a href="#" class="block animation-text max-w-sm p-6 bg-indigo-500 rounded-xl shadow-2xl hover:bg-indigo-600"
           v-for="i in 7">
           <div class="flex justify-between items-center text-xl font-medium capitalize">
             <span class="text-white">reinicio del servidor</span>
