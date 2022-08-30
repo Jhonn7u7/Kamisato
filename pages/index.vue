b3e3762cb7bc33b843bcfdb3a27329b15c359774
Erik Ciau <erikciau777@icloud.com>
2022-08-30 10:16:26 -0500

    card fix

--- pages/index.vue
+++ pages/index.vue
@@ -14,22 +14,32 @@
           </li>
         </ul>
       </div>
-      <img src="/images/hero-2.webp" class="absolute blur-sm md:blur-none -z-10 bottom-0 right-0 h-full object-cover" width="600" alt="">
+      <img src="/images/hero-2.webp" class="absolute blur-sm md:blur-none -z-10 bottom-0 right-0 h-full object-cover"
+        width="600" alt="">
     </header>
-    <br>
-    <h2 class="text-center text-indigo-700 text-lg font-bold ">Noticias</h2>
-    <p class="text-indigo-600 text-center text-lg">Lo mas recientes sobre el videojuego.<span ><a href="" class="inline-flex font-bold"> Ver mas noticias<svg-arrow-left-more-info class="block stroke-2"/></a></span></p>
-    <section class="grid grid-cols-4 space-x-1.5 m-2  "> <!--contenedor de tarjetas noticias-->
-       <div class="p-6 bg-indigo-500 rounded-xl shadow-2xl max-ws-sm  hover:bg-indigo-600">  <!--card init -->
-      <div class="flex justify-between items-center text-xl font-medium capitalize">
-        <span class="text-white">reinicio del servidor</span>
-        <span class="text-gray-200 text-sm">reciente</span>
+    <section id="main" class="my-5">
+      <header class="text-center space-y-4 text-indigo-800">
+        <h2 class="text-4xl font-bold">Noticias</h2>
+        <span class="inline-block text-xl font-extralight">Lo mas recientes sobre el videojuego</span>
+      </header>
+      <div
+        class="mt-10 mx-auto max-w-7xl px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-content-center place-items-center">
+        <a href="#"
+          class="block animation-text max-w-sm p-6 bg-indigo-500 rounded-xl shadow-2xl hover:bg-indigo-600"
+          v-for="i in 7">
+          <div class="flex justify-between items-center text-xl font-medium capitalize">
+            <span class="text-white">reinicio del servidor</span>
+            <span class="text-gray-200 text-sm">reciente</span>
+          </div>
+          <div class="space-y-2 leading-relaxed">
+            <p class="text-base font-light text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              reprehenderit soluta tempore officiis</p>
+            <a href="#" class="inline-flex justify-center items-center gap-1 capitalize text-gray-100"> Leer mas
+              <svg-arrow-left class="block stroke-2" />
+            </a>
+          </div>
+        </a>
       </div>
-      <div class="space-y-2 leading-relaxed">
-        <p class="text-base font-light text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil reprehenderit soluta tempore officiis</p>
-        <a href="#" class="inline-flex justify-center items-center gap-1 capitalize text-gray-100"> Leer mas<svg-arrow-left class="block stroke-2"/></a>
-      </div>
-       </div> <!--card final -->
-    </section > <!-- fin del contenedor de tarjetas noticias-->
+    </section>
   </NuxtLayout>
 </template>
